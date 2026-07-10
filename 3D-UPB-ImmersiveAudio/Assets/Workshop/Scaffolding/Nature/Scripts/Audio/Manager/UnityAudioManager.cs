a901f862e5f9eb51ddcb6cf8de20889aaf29f051
Rxdgxn <radugantoi@gmail.com>
2026-07-10 19:22:55 +0300

    Added footstep sounds

--- 3D-UPB-ImmersiveAudio/Assets/Workshop/Scaffolding/Nature/Scripts/Audio/Manager/UnityAudioManager.cs
+++ 3D-UPB-ImmersiveAudio/Assets/Workshop/Scaffolding/Nature/Scripts/Audio/Manager/UnityAudioManager.cs
@@ -1,7 +1,31 @@
+using System;
+using UnityEngine;
+
 namespace Workshop.Scaffolding.Nature.Scripts.Audio.Manager
 {
     public class UnityAudioManager : AudioManager
     {
-        
+        public AudioSource footstepsSource;
+        public AudioClip[] footstepSounds;
+
+        private void OnEnable()
+        {
+            fpsController.OnFootstepDetected += HandleFootsteps;
+        }
+
+        private void OnDisable()
+        {
+            fpsController.OnFootstepDetected -= HandleFootsteps;
+        }
+
+        private void HandleFootsteps(AudioUtils.AudioSurfaceType type, float speed)
+        {
+            if (type == AudioUtils.AudioSurfaceType.None)
+                return;
+
+            Debug.Log(type);
+
+            footstepsSource.PlayOneShot(footstepSounds[(int) type]);
+        }
     }
 }
